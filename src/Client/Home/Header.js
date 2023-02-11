ece33a2eb17e9ca6f8fe525196ce4d17d49dc00f
tuyen10a6 <phamtuyenok2002@gmail.com>
2023-02-11 13:35:37 +0700

    Done Header

--- src/Client/Home/Header.js
+++ src/Client/Home/Header.js
@@ -2,7 +2,9 @@ import React, { useState } from "react";
 import Dropdown from 'react-bootstrap/Dropdown';
 import DropdownButton from 'react-bootstrap/DropdownButton';
 import { FaSearch } from 'react-icons/fa';
-import { FaLocationArrow } from 'react-icons/fa';
+import { FaPhoneAlt } from 'react-icons/fa';
+import { FaShoppingCart } from 'react-icons/fa';
+import { FaSeedling } from 'react-icons/fa';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './../../Img/Image_Header/clickbuy-aar.webp';
 
@@ -10,7 +12,7 @@ const Header = () => {
     const [location, setLocation] = useState(<>Khu vực của bạn: <br /><span style={{ color: "white" }}>Miền Bắc</span></>);
     const [selectedClass, setSelectedClass] = useState("");
     const handleDropdownSelect = (selectedLocation) => {
-        setLocation(<>Khu vực của bạn: <br />{selectedLocation}</>);
+        setLocation(<>Khu vực của bạn: <br /> <span style={{ color: "White" }}>{selectedLocation}</span> </>);
         if (selectedLocation === "Miền Bắc" || selectedLocation === "Miền Nam") {
             setSelectedClass("selected");
         } else {
@@ -34,6 +36,45 @@ const Header = () => {
                     <Dropdown.Item as="button" onClick={() => handleDropdownSelect("Miền Bắc")}>Miền Bắc</Dropdown.Item>
                     <Dropdown.Item as="button" onClick={() => handleDropdownSelect("Miền Nam")}>Miền Nam</Dropdown.Item>
                 </DropdownButton>
+            </div>
+            <div style={{ display: "flex" }} className="Contact_phone_Click">
+                <a className="Link_ToPhone" href="tel:+0853675166">
+                    <div className="Icon_Phone_Header">
+                        <FaPhoneAlt />
+
+                    </div>
+                    <div className="Contact_phone">
+                        <p> Gọi mua hàng</p>
+                        <p> 0.853.675.166</p>
+                    </div>
+                </a>
+
+            </div>
+            <div style={{ display: "flex" }} className="Cart_Header">
+                <a className="Link_ToPhone" href="#">
+                    <div className="Icon_Phone_Header">
+                        <FaShoppingCart />
+
+                    </div>
+                    <div className="Contact_phone">
+                        <p> Chi tiết</p>
+                        <p style={{ fontWeight: 'bold' }}> Giỏ hàng</p>
+                    </div>
+                </a>
+
+            </div>
+            <div style={{ display: "flex" }} className="Warranty_Policy">
+                <a className="Link_ToPhone" href="#">
+                    <div className="Icon_Phone_Header">
+                        <FaSeedling />
+
+                    </div>
+                    <div className="Contact_phone">
+                        <p> Chính sách</p>
+                        <p style={{ fontWeight: 'bold' }}> Bảo Hành</p>
+                    </div>
+                </a>
+
             </div>
         </div>
     );
